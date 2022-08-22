1cf4fe5765c58933b81e3aa63728bb06b26bb567
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-22 18:42:41 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/calculatorjunithw/CalculatorJunitHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculatorjunithw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorJunitHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorJunitHwApplication.class, args);
+	}
+
+}
